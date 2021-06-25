3d5bacba4d898e2c631c17915204c4da9e473220
Alicia Zhen <43653159+aliciazhen@users.noreply.github.com>
2021-06-24 17:10:58 -0700

    Rocket Patrol!

--- scenes/src/prefabs/Rocket.js
+++
@@ -1,5 +0,0 @@
-class Rocket extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame) {
-        super(scene,)
-    }
-}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+  
 let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Menu, Play]
+    scene: [ Menu, Play ]
 }
 
 let game = new Phaser.Game(config);
 
+// set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-//reserve keyboard vars
+// reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
